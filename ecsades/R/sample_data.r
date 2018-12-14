c61ebe5896eb2c71cb4d5635501b386873af8cf0
Ye Liu <Y.Liu@hrwallingford.com>
2018-12-14 17:27:13 +0000

    Updated sample data to NOAA WAVEWATCH3 data.

---
+++ ecsades/R/sample_data.r
@@ -0,0 +1,27 @@
+#' Sample wave data by NOAA
+#'
+#' This sample dataset contains wave data (Hs and Tp) provided by the WaveWatch III project
+#' by NOAA for location 30.5N, 240E.  It contains 3-hourly data from 1 January 2006 to 
+#' 31 December 2016.
+#'
+#' @docType data
+#' 
+#' @name sample_data
+#' 
+#' @format An object of class \code{data.table} with columns \code{time}, \code{hs} and \code{tp};
+#' see \code{\link{data.table}}.
+#'
+#' @references The WAVEWATCH III Development Group., 2016:
+#' User manual and system documentation of WAVEWATCH III version 5.16.
+#' NOAA / NWS / NCEP / MMAB Technical Note 329, 326 pp. + Appendices.
+#'
+#' @source \href{http://polar.ncep.noaa.gov/waves/wavewatch/}{NOAA WAVEWATCH III Model}
+#'
+#' @examples
+#' # Loading the 3-hourly time series data
+#' data(sample_ts_10y)
+#' 
+#' # Loading the declustered event peak data
+#' data(sample_peak_10y)
+
+NULL
