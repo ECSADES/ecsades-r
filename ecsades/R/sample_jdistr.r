3ecf9b08d0befde06b36b9f0ecbbbce3082ad803
Ye Liu <y.liu@hrwallingford.com>
2019-05-14 11:41:17 +0100

    Fix issue #11.
    Changes made to sample_jdistr function rather than the stated fit_ht function.

--- ecsades/R/sample_jdistr.r
+++ ecsades/R/sample_jdistr.r
@@ -8,6 +8,10 @@
 #' and \code{\link{fit_ht}} respectively.
 #' 
 #' @param sim_year the period of the simulate data in equivalent number of years.
+#' 
+#' @param perturbed_ht_residuals whether or not to the simulation from the Heffernan-Tawn (\code{ht}) model should
+#' add a perturbation when sampling from the empirical residual distribution (default value is \code{TRUE}).  This
+#' argument is ignored when the input object is a Weibull-log-normal (\code{wln}) distribution.
 #'
 #' @return The function returns \code{data.table} object with two columns - wave
 #' height \code{hs} and wave period and \code{tp}.
@@ -20,22 +24,27 @@
 #' # Fit Heffernan-Tawn model with 0.95 marginal and dependence thresholds
 #' noaa_ht = fit_ht(data = noaa_ww3, npy = nrow(noaa_ww3)/10, p_margin_thresh = 0.95, p_dep_thresh = 0.95)
 #' 
-#' # Draw the equivalent of 100-year of data from the fitted Heffernan-Tawn model
-#' sim_ht_100y = sample_jdistr(jdistr = noaa_ht, sim_year = 100)
+#' # Draw the equivalent of 100-year of data from the fitted Heffernan-Tawn model with or without perturbed residuals
+#' sim_ht1 = sample_jdistr(jdistr = noaa_ht, sim_year = 100, perturbed_ht_residuals = FALSE)
+#' sim_ht2 = sample_jdistr(jdistr = noaa_ht, sim_year = 100)
+#' par(mfrow=c(1,2))
+#' plot(sim_ht1, pch=20, cex=.5, main="HT without perturbation")
+#' plot(sim_ht2, pch=20, cex=.5, main="HT with perturbation")
 #' 
 #' # Fit Weibull-log-normal model
 #' noaa_wln = fit_wln(data = noaa_ww3, npy = nrow(noaa_ww3)/10)
 #'
 #' # Draw the equivalent of 100-year of data from the fitted Weibull-log-normal model
-#' sim_wln_100y = sample_jdistr(jdistr = noaa_wln, sim_year = 100)
+#' sim_wln = sample_jdistr(jdistr = noaa_wln, sim_year = 100)
+#' plot(sim_wln, pch=20, cex=.5, main="WLN")
 #' 
 #' @seealso \code{\link{fit_ht}}, \code{\link{fit_wln}}
 #' 
 #' @export
-sample_jdistr = function(jdistr, sim_year){
+sample_jdistr = function(jdistr, sim_year, perturbed_ht_residuals = TRUE){
   
   if(class(jdistr)=="ht"){
-    res = .sample_ht(jdistr, sim_year)
+    res = .sample_ht(jdistr, sim_year, perturbed_ht_residuals)
   }else if(class(jdistr)=="wln"){
     res = .sample_wln(jdistr, sim_year)
   }else{
@@ -46,7 +55,7 @@ sample_jdistr = function(jdistr, sim_year){
 
 
 # Heffernan-Tawn ----------------------------------------------------------
-.sample_ht = function(ht, sim_year){
+.sample_ht = function(ht, sim_year, perturbed_ht_residuals){
   
   set.seed(.seed_sampling)
   n_sim = sim_year*ht$npy
@@ -60,9 +69,9 @@ sample_jdistr = function(jdistr, sim_year){
   n_tp = res[u_tp>ht$dep$p_dep_thresh & u_tp>=u_hs, .N]
   
   # Sample hs & tp extreme in unif
-  dep_data_hs = .sample_ht1(n_hs, ht$dep$hs$par, ht$dep$hs$resid, ht$dep$p_dep_thresh)
+  dep_data_hs = .sample_ht1(n_hs, ht$dep$hs$par, ht$dep$hs$resid, ht$dep$p_dep_thresh, perturbed_ht_residuals)
   res[u_hs>ht$dep$p_dep_thresh & u_hs>u_tp, c("u_hs", "u_tp"):=dep_data_hs[, .(u_cond, u_dep)]]
-  dep_data_tp = .sample_ht1(n_tp, ht$dep$tp$par, ht$dep$tp$resid, ht$dep$p_dep_thresh)
+  dep_data_tp = .sample_ht1(n_tp, ht$dep$tp$par, ht$dep$tp$resid, ht$dep$p_dep_thresh, perturbed_ht_residuals)
   res[u_tp>ht$dep$p_dep_thresh & u_tp>=u_hs, c("u_hs", "u_tp"):=dep_data_tp[, .(u_dep, u_cond)]]
 
   # Convert to original scale
@@ -82,7 +91,7 @@ sample_jdistr = function(jdistr, sim_year){
   return(res[, .(hs, tp)])
 }
 
-.sample_ht1 = function(n_sim, par, resid, p_dep_thresh){
+.sample_ht1 = function(n_sim, par, resid, p_dep_thresh, perturbed_ht_residuals){
   set.seed(.seed_sampling)
   a = par[1]
   b = par[2]
@@ -90,8 +99,12 @@ sample_jdistr = function(jdistr, sim_year){
   sim_data[u_cond<0.5, l_cond:=log(2*u_cond)]
   sim_data[u_cond>=0.5, l_cond:=-log(2-2*u_cond)]
   resid_max = sim_data[, (1-a)*l_cond^(1-b)]
-  resid_bw = bw.SJ(resid)
-  extended_resid = resid+rnorm(length(resid)*100, 0, resid_bw)
+  if(perturbed_ht_residuals){
+    resid_bw = bw.SJ(resid)
+    extended_resid = resid+rnorm(length(resid)*100, 0, resid_bw)
+  }else{
+    extended_resid = resid
+  }
   resid_sample = sapply(resid_max, function(x)sample(extended_resid[extended_resid<x],1))
   sim_data[, l_dep:=resid_sample*l_cond^b+a*l_cond]
   sim_data[l_dep<0, u_dep:=.5*exp(l_dep)]
